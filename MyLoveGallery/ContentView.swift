1c66b88b6cbd5792b643572d15a22da6d48571b1
enzosdev <enzohfs@gmail.com>
2026-03-10 21:19:12 -0300

    first commit

---
+++ MyLoveGallery/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyLoveGallery
+//
+//  Created by Enzo Santos on 10/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLoveGallery/MyLoveGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoveGalleryApp.swift
+//  MyLoveGallery
+//
+//  Created by Enzo Santos on 10/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyLoveGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
